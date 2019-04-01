e87ce6a0f5ac97137ec09ee1ece39165f262ab28
Dimitri Benin <dimitri.benin@gmail.com>
2019-04-01 19:04:02 +0200

    Refactor TypeScript definition to CommonJS compatible export

--- index.d.ts
+++ index.d.ts
@@ -1,59 +1,73 @@
 import {LiteralUnion} from 'type-fest';
 
-export interface Options {
-	/**
-	Include `Object.prototype` properties like `isPrototypeOf`.
+declare namespace allKeys {
+	interface Options {
+		/**
+		Include `Object.prototype` properties like `isPrototypeOf`.
 
-	@default true
-	*/
-	readonly includeObjectPrototype?: boolean;
+		@default true
+		*/
+		readonly includeObjectPrototype?: boolean;
 
-	/**
-	Include [`Symbol`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol) keys.
+		/**
+		Include [`Symbol`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol) keys.
 
-	@default true
-	*/
-	readonly includeSymbols?: boolean;
+		@default true
+		*/
+		readonly includeSymbols?: boolean;
+	}
 }
 
-/**
-Get all property keys of an object including non-enumerable and inherited ones. Like [Reflect.ownKeys()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Reflect/ownKeys) but traverses up the prototype-chain.
+declare const allKeys: {
+	/**
+	Get all property keys of an object including non-enumerable and inherited ones. Like [Reflect.ownKeys()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Reflect/ownKeys) but traverses up the prototype-chain.
 
-@returns All property names from `object`.
+	@returns All property names from `object`.
 
-@example
-```
-import allKeys from 'all-keys';
+	@example
+	```
+	import allKeys = require('all-keys');
 
-Object.getOwnPropertyNames(Symbol.prototype);
-/*
-[
-	'constructor',
-	'toString',
-	'valueOf'
-]
-*\/
+	Object.getOwnPropertyNames(Symbol.prototype);
+	/*
+	[
+		'constructor',
+		'toString',
+		'valueOf'
+	]
+	*\/
 
-allKeys(Symbol.prototype);
-/*
-Set {
-	'constructor',
-	'toString',
-	'valueOf',
-	'toLocaleString',
-	'hasOwnProperty',
-	'isPrototypeOf',
-	'propertyIsEnumerable',
-	'__defineGetter__',
-	'__lookupGetter__',
-	'__defineSetter__',
-	'__lookupSetter__',
-	'__proto__'
-}
-*\/
-```
-*/
-export default function allKeys<ObjectType extends {[key: string]: unknown}>(
-	object: ObjectType,
-	options?: Options
-): Set<LiteralUnion<keyof ObjectType, string | number | symbol>>;
+	allKeys(Symbol.prototype);
+	/*
+	Set {
+		'constructor',
+		'toString',
+		'valueOf',
+		'toLocaleString',
+		'hasOwnProperty',
+		'isPrototypeOf',
+		'propertyIsEnumerable',
+		'__defineGetter__',
+		'__lookupGetter__',
+		'__defineSetter__',
+		'__lookupSetter__',
+		'__proto__'
+	}
+	*\/
+	```
+	*/
+	<ObjectType extends {[key: string]: unknown}>(
+		object: ObjectType,
+		options?: allKeys.Options
+	): Set<LiteralUnion<keyof ObjectType, string | number | symbol>>;
+
+	// TODO: Remove this for the next major release, refactor the whole definition to:
+	// declare function allKeys<ObjectType extends {[key: string]: unknown}>(
+	// 	object: ObjectType,
+	// 	options?: allKeys.Options
+	// ): Set<LiteralUnion<keyof ObjectType, string | number | symbol>>;
+	// export = allKeys;
+	default: typeof allKeys;
+};
+
+export = allKeys;
